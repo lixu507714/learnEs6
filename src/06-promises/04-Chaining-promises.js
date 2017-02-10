ffa4257aa3ce9f04f4fa0da6517b54d1f9d26622
lixu <lx507714@163.com>
2017-02-10 19:03:09 +0800

    chaining-promise

--- src/06-promises/04-Chaining-promises.js
+++ src/06-promises/04-Chaining-promises.js
@@ -1,3 +1,17 @@
 /**
  * Created by lixu on 17/2/10.
  */
+function waitAsecond(seconds) {
+    return new Promise(function (resolve, reject) {
+        setTimeout(function () {
+            seconds++;
+            resolve(seconds);
+        }, 1000)
+    })
+}
+
+waitAsecond(0)
+    .then(waitAsecond)
+    .then(function (seconds) {
+        console.log(seconds);
+    });
