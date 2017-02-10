902143d4ff7b3230d12c8b2bb6996cec33f51893
lixu <lx507714@163.com>
2017-02-10 18:45:00 +0800

    rejecting-promise

--- src/06-promises/03-Rejecting-promises.js
+++ src/06-promises/03-Rejecting-promises.js
@@ -1,3 +1,16 @@
 /**
  * Created by lixu on 17/2/10.
  */
+
+let promise = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+        // resolve('Done!');
+        reject('Failed!');
+    }, 1000);
+});
+
+promise.then(function (value) {
+    console.log(value);
+},function(error){
+    console.log(error);
+});
