b6923ebd3b3e23f34e6137c2070062cbdcb0ada1
lixu <lx507714@163.com>
2017-02-10 19:09:40 +0800

    catch

--- src/06-promises/05-Catch.js
+++ src/06-promises/05-Catch.js
@@ -1,3 +1,25 @@
 /**
  * Created by lixu on 17/2/10.
  */
+
+// 捕获
+function waitAsecond(seconds) {
+    return new Promise(function (resolve, reject) {
+        if(seconds > 2){
+            reject('Reject!');
+        }
+        setTimeout(function () {
+            seconds++;
+            resolve(seconds);
+        }, 1000)
+    })
+}
+
+waitAsecond(2)
+    .then(waitAsecond)
+    .then(function (seconds) {
+        console.log(seconds);
+    })
+    .catch(function (err) {
+        console.log(err);
+    });
