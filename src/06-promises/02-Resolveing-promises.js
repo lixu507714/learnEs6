7e214e1dac04bfff361223864df2daea5b475dd3
lixu <lx507714@163.com>
2017-02-11 11:45:12 +0800

    promise.race and promise.all 的区别

--- src/06-promises/02-Resolveing-promises.js
+++ src/06-promises/02-Resolveing-promises.js
@@ -1,7 +1,10 @@
 /**
  * Created by lixu on 17/2/10.
  */
-//解析
+// 解析
+
+// Promise对象代表一个异步操作，有三种状态：Pending（进行中）、Resolved（已完成，又称 Fulfilled）和Rejected（已失败）。
+
 
 
 // Promise新建后立即执行，所以首先输出的是“Promise”。然后，then方法指定的回调函数，将在当前脚本所有同步任务执行完才会执行，所以“Resolved”最后输出。
--- src/06-promises/06-All-race.js
+++ src/06-promises/06-All-race.js
@@ -1,3 +1,30 @@
 /**
  * Created by lixu on 17/2/10.
  */
+
+// Promise.race方法同样是将多个Promise实例，包装成一个新的Promise实例。
+// Promise.race方法的参数与Promise.all方法一样，如果不是 Promise 实例，
+// 就会先调用下面讲到的Promise.resolve方法，将参数转为 Promise 实例，再进一步处理。
+
+    //promise.all 如果有一个失败就返回失败，
+    //promise.race 如果有一个成功就返回成功
+
+let promise1 = new Promise(function(resolve,reject){
+    setTimeout(function(){
+        resolve('Resolved');
+    },1000)
+});
+
+let promise2 = new Promise(function(resolve,reject){
+    setTimeout(function(){
+        reject('Rejected');
+    },2000)
+});
+
+Promise.race([promise1,promise2])
+    .then(function (success) {
+        console.log('success');
+    })
+    .catch(function(err){
+        console.log('err');
+    });
