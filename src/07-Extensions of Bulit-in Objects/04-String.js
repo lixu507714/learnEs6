f0956464658efb4337cd3c210f62af40d3bf1fd3
lixu <lx507714@163.com>
2017-02-13 15:15:28 +0800

    includes(), startsWith(), endsWith()

--- src/07-Extensions of Bulit-in Objects/04-String.js
+++ src/07-Extensions of Bulit-in Objects/04-String.js
@@ -1,3 +1,18 @@
 /**
  * Created by lixu on 17/2/11.
  */
+
+// includes(), startsWith(), endsWith()
+// 传统上，JavaScript只有indexOf方法，可以用来确定一个字符串是否包含在另一个字符串中。ES6又提供了三种新方法。
+// includes()：返回布尔值，表示是否找到了参数字符串。
+// startsWith()：返回布尔值，表示参数字符串是否在源字符串的头部。
+// endsWith()：返回布尔值，表示参数字符串是否在源字符串的尾部
+
+let name = "Maximilian";
+console.log(name.includes('M')); // true
+
+let names = "Maximilian";
+console.log(name.startsWith('Max')); // true
+console.log(name.startsWith('xi')); // false
+console.log(name.endsWith('an')); // true
+
