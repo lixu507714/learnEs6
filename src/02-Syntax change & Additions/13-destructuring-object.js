eac2e50147f7ceb5d3e3b4f9097cdd23d2e508e3
lixu <lx507714@163.com>
2017-02-16 19:51:32 +0800

    结构

--- src/02-Syntax change & Additions/13-destructuring-object.js
+++ src/02-Syntax change & Additions/13-destructuring-object.js
@@ -9,3 +9,23 @@ let obj = {
     }
 };
 // let {name,greet:hello} = obj;
+
+let cat = 'big';
+let dog = 'small';
+let zoo = {cat,dog};
+console.log(zoo); // { cat: 'big', dog: 'small' }
+
+
+let cat1 = 'ken';
+let dog1 = 'lili';
+let zoo1 = {cat1: cat1, dog1: dog1};
+console.log(zoo1); // { cat1: 'ken', dog1: 'lili' }
+
+
+let dogs = {
+    type:'animal',
+    many:1
+};
+
+let {type,many} = dogs;
+console.log(type,many); // animal 1
