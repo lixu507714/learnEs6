6d69d3e72e66e4111e7f0683209fa1062585ef33
lixu <lx507714@163.com>
2017-02-17 12:30:55 +0800

    变量的提升

--- src/02-Syntax change & Additions/01-Let & block scope.js
+++ src/02-Syntax change & Additions/01-Let & block scope.js
@@ -15,6 +15,37 @@ if(true){
 console.log(age);
 
 
+var a = [];
+for(let i=0;i<10;i++){
+    a[i] = function(){
+        console.log(i);
+    }
+}
+a[6]();
+
+// let 不存在变量提升
+// 变量提升 ：var命令会发生”变量提升“现象，即变量可以在声明之前使用，值为undefined；
+
+console.log(z);
+var z = 1; // undefined
+
+// console.log(x);
+// let x = 2; // 报错
+
+// var tem = 123;
+// if(true){
+//     tem = 'abc';
+//     let tem;
+// }
+
+var tem = new Date();
+function s() {
+    console.log(tem);
+    if(false){
+        var tem = '123';
+    }
+}
+s();
 
 
 
